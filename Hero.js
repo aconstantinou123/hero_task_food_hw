a273bbcefd44a07b44d5093debee0210d8496b2c
aconstantinou123 <a_constantinou@hotmail.co.uk>
2018-01-26 13:22:06 +0000

    hero can eat and favourite food gives extra boost

--- Hero.js
+++ Hero.js
@@ -1,3 +1,5 @@
+const Food = require('./Food');
+
 const Hero = function (name, favouriteFood) {
     this.name = name;
     this.health = 100;
@@ -7,6 +9,28 @@ const Hero = function (name, favouriteFood) {
 
 Hero.prototype.talk = function () {
     return `My name is ${this.name}`;
+}
+
+Hero.prototype.favouriteFoodCheck = function (food) {
+    if(this.favouriteFood === food.name){
+        return true;
+    }
+    return false;
+}
+
+
+Hero.prototype.eat = function (food) {
+    var maxHealth = 100
+    if (this.favouriteFoodCheck(food) === true && this.health < maxHealth){
+        this.health += (food.replenishmentValue * 1.5);
+    }
+
+    else if (this.health < maxHealth){
+        this.health += food.replenishmentValue;
+    }
+        if (this.health > maxHealth){
+            this.health = maxHealth;
+        }
 }
 
 module.exports = Hero;
--- specs/HeroTest.js
+++ specs/HeroTest.js
@@ -1,12 +1,17 @@
 const assert = require('assert');
 const Hero = require('../Hero');
+const Food = require('../Food');
 
 describe('Hero', function () {
 
     let hero;
+    let food1;
+    let food2;
 
     beforeEach(function () {
         hero = new Hero('Captain Average', 'Clams');
+        food1 = new Food('Cheese', 10);
+        food2 = new Food('Clams', 20);
     })
 
     it('can get heroes name', function () {
@@ -28,5 +33,29 @@ describe('Hero', function () {
     it('hero can talk and say their name', function () {
         assert.strictEqual(hero.talk(), 'My name is Captain Average');
     })
+
+    it('hero can eart', function () {
+        hero.health = 50;
+        hero.eat(food1);
+        assert.strictEqual(hero.health, 60);
+    })
+
+    it('heroes health cant go over 100', function () {
+        hero.health = 95;
+        hero.eat(food1);
+        assert.strictEqual(hero.health, 100);
+    })
+
+    it('can check favourite food', function () {
+        assert.strictEqual(hero.favouriteFoodCheck(food1), false);
+        assert.strictEqual(hero.favouriteFoodCheck(food2), true);
+    })
+
+    it ('heroes favourite food boost health 1.5 times', function () {
+        hero.health = 50;
+        hero.eat(food2);
+        assert.strictEqual(hero.health, 80);
+    })
+
 
 })
